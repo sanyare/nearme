03dabfff578f3cfdf3f56792c20a95919d15c519
Amina <kalandarovaaminakhon@gmail.com>
2024-07-30 14:35:10 -0400

    starting bar

---
+++ nearme/mainPageNearMe.swift
@@ -0,0 +1,18 @@
+//
+//  mainPageNearMe.swift
+//  nearme
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct mainPageNearMe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mainPageNearMe()
+}
