9828ae8ae743637c30d34fdacc28822779d910e9
Sanya Reddy <sanyasreddy@gmail.com>
2024-07-30 12:45:48 -0400

    Initial Commit

---
+++ nearme/nearmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  nearmeApp.swift
+//  nearme
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct nearmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
